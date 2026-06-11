39a89c080467a9a9a8d5d5ba0d4aa7b0b58579e3
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-11 19:10:09 +0530

    Update Online_Food_Order_Notification.java

--- Multithreading/Online_Food_Order_Notification.java
+++ Multithreading/Online_Food_Order_Notification.java
@@ -1,94 +1,17 @@
-/*
-Create:
+public class Message {
+	public static void main(String[] args) {
 
-Interface
-NotificationService
+	    // Constructor Injection with EmailNotification
+	    NotificationService email = new EmailNotification();
+	    OrderService order1 = new OrderService(email);
+	    order1.placeOrder();
 
-Method:
+	    System.out.println();
 
-sendMessage()
+	    // Constructor Injection with SMSNotification
+	    NotificationService sms = new SMSNotification();
+	    OrderService order2 = new OrderService(sms);
+	    order2.placeOrder();
+	}
+	}
 
-Implement:
-
-EmailNotification
-SMSNotification
-
-Create:
-
-OrderService
-Requirements
-
-Inject notification service using:
-
-Constructor Injection
-When order is placed:
-
-Order Confirmed
-Notification Sent
-Concepts Tested
-Dependency Injection
-Interface
-Loose Coupling
-
-Instructions -
-Create Interface NotificationService
-
-    Method:
-        sendMessage()
-
-
-Create Class EmailNotification
-
-    Implement NotificationService
-
-    Override sendMessage()
-
-        Print:
-            "Email Notification Sent"
-
-
-Create Class SMSNotification
-
-    Implement NotificationService
-
-    Override sendMessage()
-
-        Print:
-            "SMS Notification Sent"
-
-
-Create Class OrderService
-
-    Variable:
-        NotificationService notificationService
-
-    Constructor(NotificationService notificationService)
-
-        this.notificationService = notificationService
-
-    Method placeOrder()
-
-        Print:
-            "Order Confirmed"
-
-        notificationService.sendMessage()
-
-
-Main Method
-
-    Create EmailNotification object
-
-    Create OrderService object
-    Inject EmailNotification through constructor
-
-    Call placeOrder()
-
-
-    Create SMSNotification object
-
-    Create OrderService object
-    Inject SMSNotification through constructor
-
-    Call placeOrder()
-
-*/
