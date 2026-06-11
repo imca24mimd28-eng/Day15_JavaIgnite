6acf530acd72565eb2dd6aed990df2e03b67fe9b
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-11 19:11:22 +0530

    Update TicketBookingSystem.java

--- Multithreading/TicketBookingSystem.java
+++ Multithreading/TicketBookingSystem.java
@@ -1,81 +1,16 @@
-/*
-Ticket Booking System
+public class BookingSystem {
+	 public static void main(String[] args) {
 
-A movie theater has:
+	        Theater theater = new Theater();
 
-Total Seats = 5
+	        BookingThread t1 = new BookingThread(theater, 2, "Thread1");
+	        BookingThread t2 = new BookingThread(theater, 2, "Thread2");
+	        BookingThread t3 = new BookingThread(theater, 2, "Thread3");
 
-Create:
-
-Multiple booking threads
-Requirements
-Each thread tries to book seats.
-If seats are available, booking succeeds.
-Otherwise print:
-Booking Failed
-
-Concepts Tested
-Threads
-Shared Resource
-Synchronized Method
-
-Instructions - 
-Create class Theater
-
-    Variable:
-        totalSeats = 5
-
-    synchronized method bookSeat(int seats)
-
-        If totalSeats >= seats
-
-            Print:
-                ThreadName + " Booking Successful"
-
-            totalSeats = totalSeats - seats
-
-            Print remaining seats
-
-        Else
-
-            Print:
-                ThreadName + " Booking Failed"
+	        t1.start();
+	        t2.start();
+	        t3.start();
+	    }
+	}
 
 
-Create class BookingThread extends Thread
-
-    Theater theater
-    int seatsRequired
-
-    Constructor receives:
-        theater
-        seatsRequired
-
-    run()
-
-        theater.bookSeat(seatsRequired)
-
-
-Main Method
-
-    Create Theater object
-
-    Create Thread1 -> wants 2 seats
-    Create Thread2 -> wants 2 seats
-    Create Thread3 -> wants 2 seats
-
-    Start Thread1
-    Start Thread2
-    Start Thread3
-
-
-Possible Output
-
-    Thread1 Booking Successful
-    Remaining Seats = 3
-
-    Thread2 Booking Successful
-    Remaining Seats = 1
-
-    Thread3 Booking Failed
-*/
