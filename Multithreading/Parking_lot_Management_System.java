4f2b49bd92ebffc9551596e24dc77927a310c725
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-11 19:10:48 +0530

    Update Parking_lot_Management_System.java

--- Multithreading/Parking_lot_Management_System.java
+++ Multithreading/Parking_lot_Management_System.java
@@ -1,66 +1,21 @@
-/*
-A shopping mall has:
+public class ShoppingMall {
+	public static void main(String[] args) {
 
-Available Parking Slots = 3
+        ParkingLot parkingLot = new ParkingLot();
 
-Multiple cars arrive simultaneously.
+        CarThread car1 = new CarThread(parkingLot, "Car 1");
+        CarThread car2 = new CarThread(parkingLot, "Car 2");
+        CarThread car3 = new CarThread(parkingLot, "Car 3");
+        CarThread car4 = new CarThread(parkingLot, "Car 4");
+        CarThread car5 = new CarThread(parkingLot, "Car 5");
+        CarThread car6 = new CarThread(parkingLot, "Car 6");
 
-Each car is represented by a separate thread.
+        car1.start();
+        car2.start();
+        car3.start();
+        car4.start();
+        car5.start();
+        car6.start();
+    }
+}
 
-Requirements
-Create a shared ParkingLot class.
-Initially:
-slots = 3
-When a car arrives:
-If a slot is available:
-Park the car
-Reduce available slots
-Print:
-Car 1 Parked
-Available Slots: 2
-Otherwise:
-No Parking Available for Car 4
-Use synchronization to prevent incorrect slot allocation.
-Create at least 6 car threads.
-
-Instructions - 
-Create Class ParkingLot
-
-    availableSlots ← 3
-
-    synchronized Method parkCar(carName)
-
-        IF availableSlots > 0
-
-            Print carName + " Parked"
-
-            availableSlots ← availableSlots - 1
-
-            Print "Available Slots: " + availableSlots
-
-        ELSE
-
-            Print "No Parking Available for " + carName
-
-        END IF
-
-
-Create Class CarThread
-
-    ParkingLot parkingLot
-    carName
-
-    Method run()
-
-        parkingLot.parkCar(carName)
-
-
-Main Method
-
-    Create ParkingLot object
-
-    Create Car1 thread till Car6 thread
-    Start Car1 till  Car6
-
-
-*/
